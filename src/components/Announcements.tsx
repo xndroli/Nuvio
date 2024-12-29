d2072200b49e9f8c1fae357f02038e1ff5f9c3cd
xndroli <103154250+xndroli@users.noreply.github.com>
2024-12-29 13:50:02 -0500

    fetch school announcement data

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,4 +1,30 @@
-const Announcements = () => {
+import prisma from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server";
+
+const Announcements = async () => {
+    const { userId, sessionClaims } = await auth();
+    const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+    const roleConditions = {
+        teacher: { lessons: { some: { teacherId: userId! } } },
+        student: { students: { some: { id: userId! } } },
+        parent: { students: { some: { parentId: userId! } } },
+    };
+
+    const data = await prisma.announcement.findMany({
+        take: 3,
+        orderBy: { date: "desc" },
+        where: {
+            ...(role !== "admin" && {
+                OR: [
+                    { classId: null }, 
+                    { class: roleConditions[role as keyof typeof roleConditions] || {} },
+                ],
+            }),
+        },
+    });
+
+
     return (
         <div className='bg-white p-4 rounded-md'>
             <div className='flex items-center justify-between'>
@@ -6,34 +32,36 @@ const Announcements = () => {
                 <span className='text-xs text-gray-400'>View All</span>
             </div>
             <div className='flex flex-col gap-4 mt-4'>
-                <div className='bg-skyBlue rounded-md p-4'>
-                    <div className='flex items-center justify-between'>
-                        <h2 className='font-medium'>Lorem ipsum dolor sit</h2>
-                        <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>2025-01-01</span>
+                {data[0] && <div className='flex flex-col gap-4 mt-4'>
+                    <div className='bg-skyBlue rounded-md p-4'>
+                        <div className='flex items-center justify-between'>
+                            <h2 className='font-medium'>{data[0].title}</h2>
+                            <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>{new Intl.DateTimeFormat("en-US").format(data[0].date)}</span>
+                        </div>
+                        <p className='text-sm text-gray-400 mt-1'>{data[0].description}</p>
                     </div>
-                    <p className='text-sm text-gray-400 mt-1'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus autem quisquam, placeat praesentium.</p>
-                </div>
-            </div>
-            <div className='flex flex-col gap-4 mt-4'>
-                <div className='bg-flowerPurpleLight rounded-md p-4'>
-                    <div className='flex items-center justify-between'>
-                        <h2 className='font-medium'>Lorem ipsum dolor sit</h2>
-                        <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>2025-01-01</span>
+                </div>}
+                {data[1] && <div className='flex flex-col gap-4 mt-4'>
+                    <div className='bg-flowerPurpleLight rounded-md p-4'>
+                        <div className='flex items-center justify-between'>
+                            <h2 className='font-medium'>{data[1].title}</h2>
+                            <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>{new Intl.DateTimeFormat("en-US").format(data[1].date)}</span>
+                        </div>
+                        <p className='text-sm text-gray-400 mt-1'>{data[1].description}</p>
                     </div>
-                    <p className='text-sm text-gray-400 mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, nulla error veniam vel asperiores ratione ipsum.</p>
-                </div>
-            </div>
-            <div className='flex flex-col gap-4 mt-4'>
-                <div className='bg-sunYellowLight rounded-md p-4'>
-                    <div className='flex items-center justify-between'>
-                        <h2 className='font-medium'>Lorem ipsum dolor sit</h2>
-                        <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>2025-01-01</span>
+                </div>}
+                {data[2] && <div className='flex flex-col gap-4 mt-4'>
+                    <div className='bg-sunYellowLight rounded-md p-4'>
+                        <div className='flex items-center justify-between'>
+                            <h2 className='font-medium'>{data[2].title}</h2>
+                            <span className='text-xs text-gray-400 bg-white rounded-md px-1 py-1'>{new Intl.DateTimeFormat("en-US").format(data[2].date)}</span>
+                        </div>
+                        <p className='text-sm text-gray-400 mt-1'>{data[2].description}</p>
                     </div>
-                    <p className='text-sm text-gray-400 mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit est quod exercitationem molestias adipisci asperiores, eius quam!</p>
-                </div>
+                </div>}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Announcements
+export default Announcements;
